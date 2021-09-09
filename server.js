0023f36d3c33f1eb753c1c142c6f85b0f9fb665a
Fatimah2014 <80806004+Fatimah2014@users.noreply.github.com>
2021-09-08 23:34:53 -0400

    added code to line 8 server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const compression = require("compression");
 const dotenv= require("dotenv");
 dotenv.config();
 const app = express();
-const PORT =  3000;
+const PORT = process.env.PORT || 3000;
 app.use(logger("dev"));
 
 app.use(compression());
